c6fc1a6bc80dcc81062d2c8e17bc85ffdaceb7cf
eZubia <edzh18@gmail.com>
2016-07-08 17:29:31 -0600

    Se agrega forma de encriptacion para el registro de usuarios.

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt-nodejs');
+var Usuario = require('./models/usuarios').Usuario;
+
+app.set("view engine", "jade");
+app.use(bodyParser.json()); //Application JSON
+app.use(bodyParser.urlencoded({extended:true}));// Multipart con array
+
+app.get("/", function(req,res){
+  res.render("login");
+});
+
+app.get("/registrar", function(req,res){
+  res.render("registrar");
+});
+
+app.post("/registrar", function(req,res){
+  var usuarioNuevo = new Usuario({
+    nombre: req.body.nombre,
+    paterno: req.body.paterno,
+    materno: req.body.materno,
+    email: req.body.email
+  });
+
+  usuarioNuevo.generateHash(req.body.password);
+  usuarioNuevo.save(function (err) {
+      if (err) {
+          throw err;
+      }
+      res.redirect("/");
+  });
+});
 
 app.listen(8081);
---
+++ models/usuarios.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/login-encripted');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+
+var usuarioSchema = new Schema({
+  nombre: {type:String, required:"El nombre es Obligatorio"},
+  paterno: {type:String, required:true},
+  materno: {type:String, required:true},
+  email:{type:String, required:true},
+  password:{type:String}
+});
+
+usuarioSchema.virtual("confirmarPassword").get(function(){
+  return this.otroPassword;
+}).set(function(password){
+  this.otroPassword = password;
+});
+
+usuarioSchema.methods.generateHash = function (password) {
+    this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+usuarioSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+var Usuario = mongoose.model("Usuario", usuarioSchema);
+module.exports.Usuario = Usuario;
